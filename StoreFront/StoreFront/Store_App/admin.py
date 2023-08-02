bb3085e52ae2b3ae97f225c9e522edd330cbdbb9
Stilyan Rogov <stenli008@gmail.com>
2023-08-02 04:06:42 +0300

    changing models again

--- StoreFront/StoreFront/Store_App/admin.py
+++ StoreFront/StoreFront/Store_App/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from StoreFront.Store_App.models import Product, ProductCategory, Customer, OrderStatus
+from StoreFront.Store_App.models import Product, ProductCategory, Customer, OrderStatus, OrderItem, Order
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
 admin.site.register(Customer)
 admin.site.register(OrderStatus)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
 
 
