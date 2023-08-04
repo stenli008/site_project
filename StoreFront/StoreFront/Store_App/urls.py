7a4063fe82e7e411efa04e274117181f251e567f
Stilyan Rogov <stenli008@gmail.com>
2023-08-04 14:41:22 +0300

    reseting project

--- StoreFront/StoreFront/Store_App/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = (
-    path('1/', store, name='store'),
-)
-
--- StoreFront/StoreFront/Store_App/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def store(request):
-    return render(request, 'core/store.html')
-
