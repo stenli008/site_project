b8145939911b250908c78a223c3feef27f0318e3
Stilyan Rogov <stenli008@gmail.com>
2023-08-09 20:37:57 +0300

    added product descrioption and view page

--- Web_Store/Web_Store/common/models.py
+++ Web_Store/Web_Store/common/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
     product_picture = models.ImageField(upload_to='product_pics/', blank=True, null=True)
+    description = models.TextField(max_length=300, blank=True, null=True)
 
     def __str__(self):
         return f'{self.category.name} - {self.name}'
