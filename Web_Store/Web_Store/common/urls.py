3bb25cffc6436afb07285abc114d48e7b38973d0
Stilyan Rogov <stenli008@gmail.com>
2023-08-09 18:12:40 +0300

    added most of the cart and storefront page functionality

--- Web_Store/Web_Store/common/urls.py
+++ Web_Store/Web_Store/common/urls.py
@@ -5,5 +5,7 @@ from Web_Store.common import views
 urlpatterns = (
     path('', views.store_view, name='store'),
     path('cart/<int:pk>/', views.cart_view, name='cart'),
+    path('add_to_cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
+    path('product/<int:pk>', views.product_view, name='product')
 
 )
--- Web_Store/Web_Store/common/views.py
+++ Web_Store/Web_Store/common/views.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from Web_Store.common.models import Product, Order
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from Web_Store.common.models import Product, Order, OrderItem
 from Web_Store.accounts.models import CustomerUser
 
 
 def store_view(request):
     products = Product.objects.all()
+    user = request.user
     context = {
         'products': products,
+        'user': user,
     }
     return render(request, 'core/store.html', context)
 
@@ -23,3 +27,29 @@ def cart_view(request, pk):
         'items': items,
     }
     return render(request, 'core/cart.html', context)
+
+
+@login_required
+def add_to_cart(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    customer = request.user
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    order_item, item_created = OrderItem.objects.get_or_create(product=product, order=order)
+
+    if not item_created:
+        order_item.quantity += 1
+    else:
+        order_item.quantity += 1
+
+    order_item.save()
+
+    return redirect('store')
+
+
+def product_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'core/product_page.html', context)
