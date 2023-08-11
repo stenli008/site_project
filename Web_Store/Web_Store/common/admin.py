b58486c97e2d67798a66f4cb204a7a6d93af90d8
Stilyan Rogov <stenli008@gmail.com>
2023-08-11 20:41:50 +0300

    more admin options

--- Web_Store/Web_Store/common/admin.py
+++ Web_Store/Web_Store/common/admin.py
@@ -4,7 +4,16 @@ from django.contrib import admin
 admin.site.register(ProductCategory)
 admin.site.register(Product)
 admin.site.register(Order)
-admin.site.register(OrderItem)
+
 admin.site.register(Shipping)
 
 
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'order', 'quantity', 'date_added', 'get_total')
+    list_filter = ('order', 'product')
+    search_fields = ('product__name', 'order__id')
+    ordering = ('-date_added',)
+    actions = ['mark_as_processed']
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
